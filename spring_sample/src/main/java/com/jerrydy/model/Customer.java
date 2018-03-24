30ab6da14396b1d9de14008722e4502be3d5196a
JC (Joshua Caleb) Dy <jcdy@berkeley.edu>
2018-03-24 02:42:04 -0700

    Made Customer file

---
+++ spring_sample/src/main/java/com/jerrydy/model/Customer.java
@@ -0,0 +1,25 @@
+package com.jerrydy.model;
+
+/**
+ * Created by jcdy on 3/24/18.
+ */
+public class Customer {
+    private String firstName;
+    private String lastName;
+    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
